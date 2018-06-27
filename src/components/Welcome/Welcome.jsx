9bfe10fb95ba406c59a5ec6af6b84befd5d963aa
Ryan Santos <ryansantos86@gmail.com>
2018-06-26 23:10:04 -0700

    commit

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 export default class Welcome extends Component {
   render() {
     return (
-      <div data-aos="fade-right">
-        <h1>Ryan Santos</h1>
-        <h3>Web Developer</h3>
-        <h3>Code + Design</h3>
+      <div className="welcome" data-aos="fade-right">
+          <h1>Ryan Santos</h1>
+          <h3>Web Developer</h3>
+          <h3>Code + Design</h3>
       </div>
     );
   }
