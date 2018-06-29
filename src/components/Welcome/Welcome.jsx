8692f8fafee874e308dffff90e8f62c5b87de774
Ryan Santos <ryansantos86@gmail.com>
2018-06-28 23:27:14 -0700

    commit

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,11 +4,11 @@ export default class Welcome extends Component {
   render() {
     return (
       <div className="welcome" data-aos="fade-right">
-        <h1 className="title">Ryan Santos</h1>
-        <p className="subtitle">Web Developer</p>
-        <p className="subtitle bold">
-          Code + Design
-        </p>
+        <div className="inner">
+          <h1 className="title">Ryan Santos</h1>
+          <p className="subtitle">Web Developer</p>
+          <p className="subtitle bold">Code + Design</p>
+        </div>
       </div>
     );
   }
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -9,11 +9,23 @@ export default class ContactPage extends Component {
     return (
       <div className="page contact">
         <h4>Drop Me a Line.</h4>
-        Want to collaborate on a Project? Have a question about my work? Don't
-        hesitate!
-        <br />
-        <br />
-        <Button content="send email" className="button primary" handleClick={this.handleClick} />
+        <p>
+          I enjoy building and creating things. Some are large and some are
+          small. Some are serious and others not so much. If the project allows
+          me to grow and experiment with new technologies then I'm with it!
+        </p>
+        <p>
+          Want to collaborate on a Project? Have a question about my work? Don't
+          hesitate!
+        </p>
+        <p>
+          <i className="fas fa-phone" /> +1 310 872 6851
+        </p>
+        <Button
+          content="send email"
+          className="button primary"
+          handleClick={this.handleClick}
+        />
       </div>
     );
   }
